e157b0cf708453b23e3905dcfec79619bc572ce8
Akshat Punia <92669600+akshatpunia26@users.noreply.github.com>
2023-04-27 02:07:32 +0530

    small changes

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import matchedapp
 
 def main():
     st.title("EED374: Interactive Webapp for Matched Filtering and Radar Compression")
-    app = st.sidebar.selectbox("Select App", ["EED374: Interactive Webapp for Matched Filtering and Radar Compression", "Matched Filter App"])
+    app = st.sidebar.selectbox("Select App", ["Radar Signal Processing", "Matched Filter App"])
 
-    if app == "Radar App":
+    if app == "Radar Signal Processing":
         radarapp.main()
     else:
         matchedapp.main()
